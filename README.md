90dbcee2f42db23a574e2a83ee651cc0579a670c
anish25 <anishgandhi25@gmail.com>
2018-10-03 12:43:30 -0400

    commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# classWork1
-Inclass 
+<h1>.NET MVC Music Store</h1>
+
+<p>This site is built in COMP2084 using ASP.NET MVC, SQL Server, Entity Framework, and 
+Microsoft Azure.</p>
---
+++ fri-pm-music-store/Controllers/AlbumsController.cs
@@ -0,0 +1,30 @@
+﻿using fri_pm_music_store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fri_pm_music_store.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        // GET: Albums
+        public ActionResult Index()
+        {
+            var albums = new List<Album>();
+
+            //make a  mock list of 10 albums
+            for (int i = 1; i <= 10; i++)
+           {
+                albums.Add(new Album { Title = "Album" + 1.ToString() });
+           }
+
+            //pass mock data to view 
+
+            ViewBag.albums = albums;
+            
+            return View();
+        }
+    }
+}
